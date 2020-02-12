38893148d4c3cacfacb6735c51002c4874acd5f3
Sergey <sergey.zhmursky@gmail.com>
2020-02-12 08:42:44 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
-$(function(){
+$(function () {
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-start").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 
 
-
-  
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/rateyo/src/jquery.rateyo.css',
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -45,7 +46,9 @@ gulp.task('script', function(){
 
 gulp.task('js', function(){
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
